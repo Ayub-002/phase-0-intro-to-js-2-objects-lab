625e5a457af8b814175db9b58526cc083c46b10e
Ayub-002 <ayub.ndungu@student.moringaschool.com>
2025-03-24 14:29:25 +0300

    adds objects

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+// Initial employee object
+let employee = {
+    name: "Sam"
+  };
+  
+  // Non-destructive update function
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  // Destructive update function
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Non-destructive delete function
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Destructive delete function
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
